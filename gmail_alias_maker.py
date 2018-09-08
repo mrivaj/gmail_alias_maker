fdccaf961806e315d5ebafe78f6a69865b93d508
Javier Santana <javiersantanagodoy@gmail.com>
2018-09-08 02:37:15 +0100

    Cleaning, added printing, user input and alternative input

--- gmail_alias_maker.py
+++ gmail_alias_maker.py
@@ -4,10 +4,18 @@ def make_dot_alias(username,domain):
     for i in range (1,len(username)-1):
         generated_alias.append(username[0:i] + '.' + username[i:len(username)] + '@' + domain)
     generated_alias.append(username[0:len(username)-1] + '.' + username[-1] + '@' + domain)
-    print("Alias generated: " + str(len(generated_alias)))
-    
-mail = "yourmail@javi.com"
+
+def print_generated_alias():
+    for alias in generated_alias:
+        print(alias)
+
+mail = input("Write your gmail account\n")
+
+if mail == "":
+    mail = "test@fake.com"
 
 username, domain = mail.split('@')[0], mail.split('@')[1]
 
+print("\nStarting alias creation...\n")
 make_dot_alias(username,domain)
+print_generated_alias()
